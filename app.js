7d29ca2cee3600910ef52506d0358f8d2fdfafba
Manish-Baghel29 <mbaghel699@gmail.com>
2025-06-24 22:11:14 +0530

    Adding High Score

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ let btns=['red','green','yellow','purple'];
 
 let started = false;
 let level = 0;
+let hS=0;
 let h2=document.querySelector('h2');
 let strBtn = document.querySelector('.start');
 
 strBtn.addEventListener("click" , function(){
     if(started == false){
         started = true;
+        hS++;
         levelUp();
     }
 });
@@ -31,9 +33,9 @@ function userFlash(btn){
 function levelUp(){
     userSeq=[];
     level++;
-    h2.innerText = `Level ${level}`;
+    h2.innerText = `Level ${level}   Highest Score : ${hS}`;
 
-    let randomIndex = Math.floor(Math.random()*3);
+    let randomIndex = Math.floor(Math.random()*4);
     let randomColor = btns[randomIndex];
     let randomBtn = document.querySelector(`.${randomColor}`);
     gameSeq.push(randomColor);
@@ -46,14 +48,26 @@ function checkAns(idx) {
     if(userSeq[idx]===gameSeq[idx]){
         if(userSeq.length == gameSeq.length){
             setTimeout(levelUp , 1000);
+            if(hS < level){
+                hS++;
+            }
         }
     } else{
-        h2.innerHTML=`Game Over! Your score was <b>${level}</b> <br> Click on start button to restart.`;
-        document.querySelector("body").style.backgroundColor="red";
-        setTimeout(function(){
+        if(hS >= level){
+            h2.innerHTML=`Game Over! Your score was <b>${level}</b> <br> Click on start button to restart.`;
+            document.querySelector("body").style.backgroundColor="red";
+            setTimeout(function(){
             document.querySelector("body").style.backgroundColor="white";
-        },200);
-        reSet();
+            },200);
+            reSet();
+        }else{
+            h2.innerHTML=`Game Over! Congratulations!! now you have your <b>Highest score : ${level}</b> <br> Click on start button to restart.`;
+            document.querySelector("body").style.backgroundColor="red";
+            setTimeout(function(){
+            document.querySelector("body").style.backgroundColor="white";
+            },200);
+            reSet();
+        }
     }
 }
 
